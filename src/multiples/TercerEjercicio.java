4bdc745119b7c417e9da6364e47a915f4b55f217
Santiago <tabordasantiago350@gmail.com>
2024-07-29 01:21:15 -0500

    Cuarto ejercicio

---
+++ src/multiples/TercerEjercicio.java
@@ -0,0 +1,31 @@
+package multiples;
+
+public class TercerEjercicio {
+	
+
+	    public static void main(String[] args) {
+	        int categoria = 2; 
+
+	        switch (categoria) {
+	            case 1:
+	                System.out.println("Coche");
+	                break;
+	            case 2:
+	                System.out.println("Motocicleta");
+	                break;
+	            case 3:
+	                System.out.println("Camión");
+	                break;
+	            case 4:
+	                System.out.println("Autobús");
+	                break;
+	            default:
+	                System.out.println("Categoría de vehículo desconocida");
+	                break;
+	        }
+	    }
+	
+
+	
+
+}
